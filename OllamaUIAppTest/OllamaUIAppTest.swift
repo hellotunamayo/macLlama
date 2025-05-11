1f58592dbf9b30a4402dd567ed9a807bfe4f664e
Min Yoo <yoo@minyoungyoo.com>
2025-05-11 19:39:17 +0900

    Added Unit Test Target

---
+++ OllamaUIAppTest/OllamaUIAppTest.swift
@@ -0,0 +1,17 @@
+//
+//  OllamaUIAppTest.swift
+//  OllamaUIAppTest
+//
+//  Created by Minyoung Yoo on 5/11/25.
+//
+
+import Testing
+import OllamaUIApp
+
+struct OllamaUIAppTest {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
