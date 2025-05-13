0e6bcd93642e3b750b87f724f19708f32ee161dd
Min Yoo <yoo@minyoungyoo.com>
2025-05-13 15:02:08 +0900

    feat: Add "Reload model" button in ConversationView

--- macLlama/Views/ConversationView.swift
+++ macLlama/Views/ConversationView.swift
@@ -25,6 +25,7 @@ struct ConversationView: View, OllamaNetworkServiceUser {
     @State private var currentModel: String = ""
     @State private var isThinking: Bool = false
     @State private var scrollToIndex: Int = 0
+    @State private var isModelLoading: Bool = false
     @State internal var ollamaNetworkService: OllamaNetworkService?
 
     let ollamaProfilePicture: NSImage? = NSImage(named: "llama_gray")
@@ -39,33 +40,67 @@ struct ConversationView: View, OllamaNetworkServiceUser {
             //MARK: Conversation view
             VStack {
                 if !self.modelList.isEmpty {
-                    Picker("Select a model", selection: $currentModel) {
-                        ForEach(modelList, id: \.self) { model in
-                            Text(model.name)
-                                .foregroundStyle(.primary)
-                                .tag(model.name)
+                    HStack{
+                        Picker("Current Model", selection: $currentModel) {
+                            ForEach(modelList, id: \.self) { model in
+                                Text(model.name)
+                                    .foregroundStyle(.primary)
+                                    .tag(model.name)
+                            }
                         }
+                        .pickerStyle(MenuPickerStyle())
+                        .frame(width: Units.appFrameMinWidth * 0.5)
+                        .onChange(of: currentModel) { oldValue, newValue in
+                            Task {
+                                await self.ollamaNetworkService?.changeModel(model: newValue)
+                            }
+                        }
+                        
+                        Button {
+                            withAnimation {
+                                self.isModelLoading.toggle()
+                            }
+                            
+                            Task {
+                                self.isModelLoading = true
+                                try await self.initModelList()
+                                self.isModelLoading = false
+                            }
+                        } label: {
+                            VStack {
+                                if self.isModelLoading {
+                                    Label("Loading...", systemImage: "rays")
+                                } else {
+                                    Label("Reload", systemImage: "arrow.clockwise")
+                                }
+                            }
+                            .padding(.vertical, Units.normalGap / 8)
+                            .frame(width: 80)
+                        }
+                        .tint(.primary)
+                        .controlSize(.regular)
+                        .buttonStyle(.bordered)
                     }
-                    .pickerStyle(MenuPickerStyle())
                     .padding()
-                    .onChange(of: currentModel) { oldValue, newValue in
-                        Task {
-                            await self.ollamaNetworkService?.changeModel(model: newValue)
-                        }
-                    }
+                    
+                    Divider()
+                        .foregroundStyle(Color(nsColor: .systemGray))
+                        .opacity(self.colorScheme == .dark ? 1.0 : 0.9)
                 }
                 
                 //MARK: Chat Scroll View
                 ScrollViewReader { proxy in
                     ScrollView {
                         ForEach(0..<self.chatHistory.count, id: \.self) { index in
-                            Divider()
-                                .foregroundStyle(Color(nsColor: .systemGray))
-                                .opacity(self.colorScheme == .dark ? 1.0 : 0.9)
                             
                             ChatBubbleView(chatData: chatHistory[index])
                                 .padding()
                                 .id(index)
+                            
+                            Divider()
+                                .foregroundStyle(Color(nsColor: .systemGray))
+                                .opacity(self.colorScheme == .dark ? 1.0 : 0.9)
+                            
                         }
                     }
                     .task {
