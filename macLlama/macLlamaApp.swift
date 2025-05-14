27b239b867ad5bf5578c7db4a0c4a731d87ff6ec
Min Yoo <yoo@minyoungyoo.com>
2025-05-14 15:55:09 +0900

    Feat: Add Menu Bar icon & menu

--- macLlama/macLlamaApp.swift
+++ macLlama/macLlamaApp.swift
@@ -33,5 +33,54 @@ struct macLlamaApp: App {
             .frame(minWidth: Units.appFrameMinWidth, idealWidth: Units.appFrameMinWidth,
                    minHeight: Units.appFrameMinHeight, idealHeight: Units.appFrameMinHeight)
         }
+        
+        MenuBarExtra("macLlama", image: "macLlama-menuIcon") {
+            let versionString: Any = Bundle.main.infoDictionary?["CFBundleShortVersionString"] ?? ""
+            let buildString: Any = Bundle.main.infoDictionary?["CFBundleVersion"] ?? ""
+            Text("macLlama v\(versionString) (\(buildString))")
+            
+            Divider()
+            
+            Button {
+                Task {
+                    await ShellService.runShellScript("ollama serve")
+                }
+            } label: {
+                Text("Start Ollama Server")
+            }
+            
+            Button {
+                Task {
+                    try await ShellService.openTerminal()
+                }
+            } label: {
+                Text("Open Terminal.app")
+            }
+            
+            #if DEBUG
+            Button {
+                Task {
+                    await ShellService.runShellScript("killall ollama")
+                }
+            } label: {
+                Text("Kill Ollama server")
+                    .foregroundStyle(.red)
+            }
+            #endif
+            
+            Divider()
+            
+            Link("Check for Update", destination: URL(string: "https://github.com/hellotunamayo/macLlama/releases")!)
+            Link("macLlama on GitHub", destination: URL(string: "https://github.com/hellotunamayo/macLlama")!)
+            
+            Divider()
+            
+            Button {
+                NSApplication.shared.terminate(nil)
+            } label: {
+                Text("Quit macLlama")
+            }
+        }
+        
     }
 }
