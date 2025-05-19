a3ca46ee2645dcdcb0a28a368366cc0aaa86d98a
Min Yoo <yoo@minyoungyoo.com>
2025-05-19 16:38:47 +0900

    refactor: Add init() in ServerStatus class

--- macLlama/Others/Extras.swift
+++ macLlama/Others/Extras.swift
@@ -11,6 +11,12 @@ import Foundation
 final class ServerStatus: ObservableObject {
     @Published private(set) var indicator: Bool = false
     
+    private init() {
+        Task {
+            try await self.updateServerStatus()
+        }
+    }
+    
     ///Check server status using OllamaNetworkService.isServerOnline() and update Boolean value to ServerStatus's indicator property.
     func updateServerStatus() async throws {
         let serverStatus = try await OllamaNetworkService.isServerOnline()
