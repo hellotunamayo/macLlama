aa34595157b3b5984ae484de4ed119072f32095a
Min Yoo <yoo@minyoungyoo.com>
2025-05-26 20:44:44 +0900

    refactor: Extract updateview in independent file

--- macLlama/macLlamaApp.swift
+++ macLlama/macLlamaApp.swift
@@ -126,53 +126,7 @@ struct macLlamaApp: App {
         
         //MARK: New version available
         Window("macLlama update", id: "updateWindow") {
-            VStack(alignment: .leading) {
-                HStack {
-                    Image("macLlama-profile")
-                        .resizable()
-                        .frame(width: 40, height: 40)
-                        .aspectRatio(contentMode: .fit)
-                        .clipShape(Circle())
-                        .padding(.trailing, 3)
-                    
-                    Text("New macLlama is available!")
-                        .font(.largeTitle)
-                        .fontWeight(.bold)
-                }
-                .padding(.bottom, 10)
-                
-                Text("We’ve released a **new version of macLlama!** To enjoy the latest features and improvements, please update your app now.")
-                    .font(.title3)
-                    .padding(.bottom, 10)
-                
-                Divider()
-                
-                ScrollView {
-                    VStack {
-                        Markdown{
-                            MarkdownContent(self.updateData.body)
-                        }
-                        .padding()
-                    }
-                }
-                .frame(height: 500)
-                
-                Divider()
-                
-                Spacer()
-                
-                VStack {
-                    Link(destination: URL(string: self.updateData.htmlURL) ?? URL(fileURLWithPath: "")) {
-                        Label("Click here to update", systemImage: "safari")
-                    }
-                    .buttonStyle(.borderedProminent)
-                    .controlSize(.large)
-                    .padding(.vertical)
-                }
-                .frame(maxWidth: .infinity, alignment: .trailing)
-            }
-            .frame(width: 500, height: 700)
-            .padding()
+            UpdatePanelView(updateData: $updateData)
         }
         .windowResizability(.contentSize)
     }
