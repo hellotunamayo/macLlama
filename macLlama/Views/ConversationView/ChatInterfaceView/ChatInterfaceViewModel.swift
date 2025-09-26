64c38a6c8c285e9440a6f71c9ca18804ef30b12d
Min Yoo <yoo@minyoungyoo.com>
2025-09-26 20:07:38 +0900

    refactor: Initial Commit

--- macLlama/Views/ConversationView/ChatInterfaceView/ChatInterfaceViewModel.swift
+++ macLlama/Views/ConversationView/ChatInterfaceView/ChatInterfaceViewModel.swift
@@ -15,14 +15,21 @@ struct SummaryResult {
     var summaryText: String
 }
 
+@available(macOS 26.0, *)
+@Generable
+struct MostRelevantURL {
+    @Guide(description: "This is most relavant web URL from google search.")
+    var urlString: String
+}
+
 actor ChatInterfaceViewModel {
     private let searchService = GoogleSearchService()
+    private let googleSearchActor = GoogleSearchService()
     
     func getWebResponse(from prompt: String) async -> (String,String)? {
-        let actor = GoogleSearchService()
         do {
             let userPrompt: String = prompt
-            guard let result = try await actor.fetchSearchResults(for: userPrompt) else { return nil }
+            guard let result = try await googleSearchActor.fetchSearchResults(for: userPrompt) else { return nil }
             
             #if DEBUG
             print("User's prompt is: \(prompt)")
@@ -35,18 +42,21 @@ actor ChatInterfaceViewModel {
             guard let items = result.items,
                   let link = items[0].link,
                   let url = URL(string: link) else { return nil }
-            guard let firstDataFromFirstURL = await actor.getUrlContents(from: url) else { return nil }
-            
-            let filteredString = await actor.extractAllContentFromTags(tags: ["h1", "p", "table", "td", "tr"], from: firstDataFromFirstURL)
-            let tagStrippedString = filteredString.replacingOccurrences(of: "<[^>]+>", with: "", options: .regularExpression)
-            let whiteSpaceRemovedString = tagStrippedString.replacingOccurrences(of: "\\s+", with: " ", options: .regularExpression)
             
             if #available(macOS 26.0, *) {
+                //Apple Foundation Model Support
+                let relevantLink = try await getMostRelevantURLString(userPrompt: prompt, searchResult: result)
+                let relevantURL = URL(string: relevantLink)
+                let firstDataFromFirstURL = await googleSearchActor.getUrlContents(from: relevantURL!) ?? ""
+                let whiteSpaceRemovedString = await getWhiteSpaceRemovedText(content: firstDataFromFirstURL)
+                
                 let summaryResponse = try await self.summrizeSearchResult(userPrompt: userPrompt,
                                                                           content: whiteSpaceRemovedString)
-                return (summaryResponse, result.items?[8].link ?? "No data")
+                
+                return (summaryResponse, relevantLink)
             } else {
-//                let maxLength: Int = 4_000
+                let firstDataFromFirstURL = await googleSearchActor.getUrlContents(from: url) ?? ""
+                let whiteSpaceRemovedString = await getWhiteSpaceRemovedText(content: firstDataFromFirstURL)
                 let truncatedText = await self.truncateText(whiteSpaceRemovedString)
                 return (truncatedText, result.items?[8].link ?? "No data")
             }
@@ -54,6 +64,14 @@ actor ChatInterfaceViewModel {
             debugPrint(error)
             return nil
         }
+    }
+    
+    private func getWhiteSpaceRemovedText(content: String) async -> String {
+        let filteredString = await googleSearchActor.extractAllContentFromTags(tags: ["h1", "p", "table", "td", "tr"], from: content)
+        let tagStrippedString = filteredString.replacingOccurrences(of: "<[^>]+>", with: "", options: .regularExpression)
+        let whiteSpaceRemovedString = tagStrippedString.replacingOccurrences(of: "\\s+", with: " ", options: .regularExpression)
+        
+        return whiteSpaceRemovedString
     }
     
     @available(macOS 26.0, *)
@@ -97,6 +115,26 @@ actor ChatInterfaceViewModel {
         #endif
         
         return combinedSummary
+    }
+    
+    @available(macOS 26.0, *)
+    private func getMostRelevantURLString(userPrompt: String, searchResult: SearchResult) async throws -> String {
+        let foundationModelPrompt: String = "Select the most relevant URL from the provided URLs `\(searchResult)` based on the user prompt `\(userPrompt)`. Respond with only the URL text."
+        var urls: [String] = []
+        searchResult.items?.forEach { result in
+            if let url = result.link {
+                urls.append(url)
+            }
+        }
+        let session = LanguageModelSession()
+        let selectedURL = try await session.respond(to: foundationModelPrompt, generating: MostRelevantURL.self)
+        
+        #if DEBUG
+        debugPrint("Most relevant URL is : \(selectedURL.content.urlString)")
+        debugPrint("Search Result's url: \(urls)")
+        #endif
+        
+        return selectedURL.content.urlString
     }
     
     private func truncateText(_ text: String, maxLength: Int? = nil) async -> String {
